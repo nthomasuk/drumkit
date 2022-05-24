0a4693d4f4403ec8fda60b9f2026d5dfd9deec0b
ntcreationsuk <66379881+ntcreationsuk@users.noreply.github.com>
2022-05-24 23:52:45 +0100

    adds refactoring and animation

--- app.js
+++ app.js
@@ -17,96 +17,46 @@ const snareAudio = new Audio("/sounds/snare.wav");
 const tinkAudio = new Audio("/sounds/tink.wav");
 const tomAudio = new Audio("/sounds/tom.wav");
 
-boomElem.addEventListener('click', () => {
-    boomAudio.pause();
-    boomAudio.currentTime = 0;
-    boomAudio.play();
-})
+const playSound = (audio, element) => {
+    animate(element);
+    audio.pause();
+    audio.currentTime = 0;
+    audio.play();
+};
 
-clapElem.addEventListener('click', () => {
-    clapAudio.pause();
-    clapAudio.currentTime = 0;
-    clapAudio.play();
-})
+const animate = (element) => {
+    if (!element || !element.style) return;
+    element.style.border = "10px solid #2132e4"
+    element.style.margin = "10px";
+    element.style.background = "radial-gradient(#2132e4, #e66465)";
+    setTimeout(() => {
+        element.style.border = "none";
+        element.style.margin = "20px";
+        element.style.background = "radial-gradient(#e66465, #2132e4)";
+    }, 100);
+}; 
 
-hihatElem.addEventListener('click', () => {
-    hihatAudio.pause();
-    hihatAudio.currentTime = 0;
-    hihatAudio.play();
-})
-
-kickElem.addEventListener('click', () => {
-    kickAudio.pause();
-    kickAudio.currentTime = 0;
-    kickAudio.play();
-})
-
-openhatElem.addEventListener('click', () => {
-    openhatAudio.pause();
-    openhatAudio.currentTime = 0;
-    openhatAudio.play();
-})
-
-rideElem.addEventListener('click', () => {
-    rideAudio.pause();
-    rideAudio.currentTime = 0;
-    rideAudio.play();
-})
-
-snareElem.addEventListener('click', () => {
-    snareAudio.pause();
-    snareAudio.currentTime = 0;
-    snareAudio.play();
-})
-
-tinkElem.addEventListener('click', () => {
-    tinkAudio.pause();
-    tinkAudio.currentTime = 0;
-    tinkAudio.play();
-})
-
-tomElem.addEventListener('click', () => {
-    tomAudio.pause();
-    tomAudio.currentTime = 0;
-    tomAudio.play();
-})
+boomElem.addEventListener('click', () => playSound(boomAudio, boomElem));
+clapElem.addEventListener('click', () => playSound(clapAudio, clapElem));
+hihatElem.addEventListener('click', () => playSound(hihatAudio, hihatElem));
+kickElem.addEventListener('click', () => playSound(kickAudio, kickElem));
+openhatElem.addEventListener('click', () => playSound(openhatAudio, openhatElem));
+rideElem.addEventListener('click', () => playSound(rideAudio, rideElem));
+snareElem.addEventListener('click', () => playSound(snareAudio, snareElem));
+tinkElem.addEventListener('click', () => playSound(tinkAudio, tinkElem));
+tomElem.addEventListener('click', () => playSound(tomAudio, tomElem));
 
 window.addEventListener("keydown", (e) =>{
-    if(e.key == "1") {
-        boomAudio.pause();
-        boomAudio.currentTime = 0;
-        boomAudio.play();
-    } else if(e.key == "2") {
-        clapAudio.pause();
-        clapAudio.currentTime = 0;
-        clapAudio.play();
-    } else if(e.key == "3") {
-        hihatAudio.pause();
-        hihatAudio.currentTime = 0;
-        hihatAudio.play();
-    } else if(e.key == "4") {
-        kickAudio.pause();
-        kickAudio.currentTime = 0;
-        kickAudio.play();
-    } else if(e.key == "5") {
-        openhatAudio.pause();
-        openhatAudio.currentTime = 0;
-        openhatAudio.play();
-    } else if(e.key == "6") {
-        rideAudio.pause();
-        rideAudio.currentTime = 0;
-        rideAudio.play();
-    } else if(e.key == "7") {
-        snareAudio.pause();
-        snareAudio.currentTime = 0;
-        snareAudio.play();
-    } else if(e.key == "8") {
-        tinkAudio.pause();
-        tinkAudio.currentTime = 0;
-        tinkAudio.play();
-    } else if(e.key == "9") {
-        tomAudio.pause();
-        tomAudio.currentTime = 0;
-        tomAudio.play();
+    switch(e.key) {
+        case "1": playSound(boomAudio, boomElem); break;
+        case "2": playSound(clapAudio, clapElem); break;
+        case "3": playSound(hihatAudio, hihatElem); break;
+        case "4": playSound(kickAudio, kickElem); break;
+        case "5": playSound(openhatAudio, openhatElem); break;
+        case "6": playSound(rideAudio, rideElem); break;
+        case "7": playSound(snareAudio, snareElem); break;
+        case "8": playSound(tinkAudio, tinkElem); break;
+        case "9": playSound(tomAudio, tomElem); break;
+        default: return;
     }
-})
+});
